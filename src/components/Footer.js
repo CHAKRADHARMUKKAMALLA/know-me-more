bb533308768b3a761335c27cba51c2c3acc1b700
chakradhar reddy mukkamalla <76767521+CHAKRADHARMUKKAMALLA@users.noreply.github.com>
2021-08-11 19:02:50 +0530

    completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import '../css/components.css'
 function Footer() {
     return (
         <div className = "Footer container">
-            <p>Made with <span role="img" aria-label="heart"> ❤️ </span>&nbsp; by Sagnik Sahoo</p>
+            <p><font size = 7>Made with <span role="img" aria-label="heart"> ❤️ </span>&nbsp; by chakradhar mukkamalla </font></p>
         </div>
     )
 }
